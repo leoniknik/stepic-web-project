0dc734eae60df32d84e1c521e1cb10ec7cb3398e
leoniknik <leoniknik@mail.ru>
2016-12-31 22:38:18 +0000

    01:38

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -18,4 +18,11 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', 'test'),
+    url(r'^login/', 'test'),
+    url(r'^signup/', 'test'),
+    url(r'^question/(?P<num>[0-9]+/$', 'test'),
+    url(r'^ask/', 'test'),
+    url(r'^popular/', 'test'),
+    url(r'^new/', 'test'),
 ]
